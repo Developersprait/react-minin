74a42dff7a395f85501ac41bc44cb1e9cfba4acf
Leon Antoshyn <Doctorsprait@gmail.com>
2021-06-06 15:45:51 +0300

    stoped at 50.00 min

---
+++ src/todo/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AddTodo(){
+    return (
+        <form>
+            <input />
+            <button></button>
+        </form>
+    )
+}
+
+export default AddTodo
