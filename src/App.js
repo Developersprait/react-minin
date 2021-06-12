cdc820701f35b6f19555b7a794f9fdafe42ac024
Leon Antoshyn <Doctorsprait@gmail.com>
2021-06-12 20:19:56 +0300

    app finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import TodoList from './todo/TodoList'
 import Context from './context'
+// import AddTodo from './todo/AddTodo'
+import Loader from './Loader'
+import Modal from './modal/Modal'
+
+const AddTodo = React.lazy(() => import('./todo/AddTodo'))
 
 function App() {
-  const [todos, setTodos] = React.useState(
-    [
-      {id:1, completed: false, title: 'Почесать маношку'},
-      {id:2, completed: false, title: 'Почесать попсика'},
-      {id:3, completed: false, title: 'Почесать ляльку'},
-    ]
-  )
+  const [todos, setTodos] = React.useState([]);
+  const [loading, setLoading] = React.useState([true])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(response => response.json())
+    .then(todos => {
+      setTimeout(() => {
+        setTodos(todos)
+        setLoading(false)
+      }, 2000)
+      
+    })
+  },[])
+
   function toggleTodo(id){
       setTodos( todos.map(todo => {
         if(todo.id === id){
@@ -23,14 +35,27 @@ function App() {
   function removeTodo(id){
     setTodos(todos.filter(todo => todo.id !== id))
   }
+  function addTodo(title){
+    setTodos(todos.concat([{
+      title,
+      id: Date.now(),
+      completed: false
+    }]))
+  }
 
   return (
     <Context.Provider value={{removeTodo}}>
     <div className="wrapper">
       <h1>React tutorial</h1>
-      {todos.length ?<TodoList 
+      <React.Suspense fallback="...Loading">
+      <Modal />  
+      <AddTodo onCreate={addTodo}/>
+      </React.Suspense>
+      {loading && <Loader />}
+      {todos.length ?
+      (<TodoList 
       todos={todos}
-      onToggle={toggleTodo}/> : <p>No todos!</p>}
+      onToggle={toggleTodo}/>) : loading? null : (<p>No todos!</p>)}
       
     </div>
     </Context.Provider>
--- src/todo/AddTodo.js
+++ src/todo/AddTodo.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 
-function AddTodo(){
+function useInputValue(defaultValue = ''){
+    const [value, setValue] = useState(defaultValue);
+    return {
+        bind: {
+         value,
+        onChange: event => setValue(event.target.value)   
+        },
+        clear: () => setValue(''),
+        value: () => value
+    }
+}
+
+function AddTodo({ onCreate }){
+    const input = useInputValue('')
+   
+    function submitHandler(event){
+        event.preventDefault()
+        if(input.value().trim()){
+            onCreate(input.value())
+            input.clear()
+        }
+    }
     return (
-        <form>
-            <input />
-            <button></button>
+        <form style={{marginBottom:'1rem'}} onSubmit={submitHandler}>
+            <input {...input.bind}/>
+            <button type="submit">Add todo</button>
         </form>
     )
 }
